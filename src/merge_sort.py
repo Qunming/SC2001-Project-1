176bec9c1e364aae26e97c044d028a536a772658
Qunming <linq0018@e.ntu.edu.sg>
2026-09-05 17:32:32 +0800

    Add merge sort implementation

---
+++ src/merge_sort.py
@@ -0,0 +1,47 @@
+def merge_sort(A):
+
+    if len(A) <= 1:
+        return A
+
+    middle = len(A) // 2
+
+    left = A[:middle]
+    right = A[middle:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+
+
+
+def merge(left, right):
+
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+
+    return result
+
+
+
+numbers = [8, 3, 2, 9, 1, 2, 0, 1, 3, 10, -2]
+
+print(merge_sort(numbers))
